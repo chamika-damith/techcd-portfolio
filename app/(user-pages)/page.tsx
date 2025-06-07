c585ce999095deddde2de2c3e887d28e89b3b934
Dominic Brian <dominic91960@gmail.com>
2025-06-07 11:25:22 +0530

    test(home-page): sample animated button

--- app/(user-pages)/page.tsx
+++ app/(user-pages)/page.tsx
@@ -15,11 +15,43 @@ const HomePage = () => {
       <About />
       <Community />
       <Store />
-      <Image
-        src={bg}
-        alt="Banner"
-        className="h-[188px] object-cover object-center sm:h-[281.75px] md:h-[375.5px] lg:h-[469.25px] xl:h-[563px] 2xl:h-[656.75px]"
-      />
+      <div className="relative">
+        <Image
+          src={bg}
+          alt="Banner"
+          className="h-[188px] object-cover object-center sm:h-[281.75px] md:h-[375.5px] lg:h-[469.25px] xl:h-[563px] 2xl:h-[656.75px]"
+        />
+
+        <div className="bg-background absolute inset-0 flex items-center justify-center">
+          <button className="group relative rounded-xl px-[1.5em] py-[0.6em] shadow-[0_0_1em_rgba(0,132,240,0.4)] transition-all duration-500 ease-linear hover:shadow-none">
+            <span className="absolute inset-0 flex items-center justify-center rounded-xl">
+              <span className="absolute inset-0 flex items-center justify-center overflow-hidden rounded-xl opacity-0 blur-[8px] transition-all duration-500 ease-linear group-hover:opacity-100">
+                <span
+                  className="aspect-square w-full scale-110 rounded-full transition-all duration-700 ease-linear group-hover:rotate-180"
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(45deg, #0084f0, #4daaf5, transparent, transparent, #006dc1, #0084f0)",
+                  }}
+                ></span>
+              </span>
+
+              <span className="absolute inset-0 flex items-center justify-center overflow-hidden rounded-xl opacity-0 transition-all duration-500 ease-linear group-hover:opacity-100">
+                <span
+                  className="aspect-square w-full scale-110 rounded-full transition-all duration-700 ease-linear group-hover:rotate-180"
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(45deg, #0084f0, #4daaf5, transparent, transparent, #006dc1, #0084f0)",
+                  }}
+                ></span>
+              </span>
+
+              <span className="absolute inset-0 m-[2px] rounded-xl bg-black"></span>
+            </span>
+
+            <span className="font-secondary relative uppercase">HOVER</span>
+          </button>
+        </div>
+      </div>
     </>
   );
 };
