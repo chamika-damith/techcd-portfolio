e39bac975519e8e7f90575bf717dd50ae49fccbf
Dominic Brian <dominic91960@gmail.com>
2025-07-07 17:24:19 +0530

    refactor(navbar): refactor mobile nav animations and state management

--- components/user/navbar/Navbar.tsx
+++ components/user/navbar/Navbar.tsx
@@ -16,66 +16,49 @@ gsap.registerPlugin(useGSAP);
 const Navbar = () => {
   const activePath = usePathname();
   const containerRef = useRef<HTMLElement>(null);
+  const timelineRef = useRef<GSAPTimeline>(null);
+
+  const mobileNavOverlayRef = useRef<HTMLDivElement>(null);
+  const mobileNavRef = useRef<HTMLElement>(null);
+
+  const mobileNavBgRef = useRef<HTMLDivElement>(null);
+  const mobileNavListRef = useRef<HTMLUListElement>(null);
 
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-  const [displayMobileNav, setDisplayMobileNav] = useState(false);
-  const [isAnimating, setIsAnimating] = useState(false);
 
-  useGSAP(
+  const { contextSafe } = useGSAP(
     () => {
-      // Mobile nav entry timeline
-      const entryTimeline = gsap
-        .timeline({
-          paused: true,
-          onComplete: () => {
-            gsap.set(".mobile-nav-list", { overflow: "auto" });
-            setIsAnimating(false);
-          },
-        })
-        .to(".mobile-nav-bg", {
-          clipPath: "circle(120% at 50% 100%)",
+      if (
+        !mobileNavOverlayRef.current ||
+        !mobileNavBgRef.current ||
+        !mobileNavListRef.current
+      )
+        return;
+
+      const tl = gsap
+        .timeline({ paused: true })
+        .to(mobileNavOverlayRef.current, {
+          opacity: 1,
           ease: "none",
           duration: 0.7,
         })
-        .to(".mobile-nav-item", { opacity: 1, yPercent: 0, stagger: 0.1 });
-
-      // Mobile nav exit timeline
-      const exitTimeline = gsap
-        .timeline({
-          paused: true,
-          onComplete: () => {
-            setDisplayMobileNav(false);
-            setIsAnimating(false);
-          },
-        })
-        .fromTo(
-          ".mobile-nav-item",
-          { opacity: 1, yPercent: 0 },
-          { opacity: 0, yPercent: 100, stagger: 0.1 },
-        )
         .to(
-          ".mobile-nav-bg",
+          mobileNavBgRef.current,
           {
-            clipPath: "circle(0% at 50% 100%)",
+            clipPath: "circle(120% at 50% 100%)",
             ease: "none",
             duration: 0.7,
           },
-          "<+0.3",
-        );
-
-      if (isMobileNavOpen) {
-        setIsAnimating(true);
-        gsap.set(".mobile-nav-item", { opacity: 0, yPercent: 100 });
-        entryTimeline.play();
-      }
-
-      if (!isMobileNavOpen && displayMobileNav) {
-        setIsAnimating(true);
-        gsap.set(".mobile-nav-list", { overflow: "hidden" });
-        exitTimeline.play();
-      }
+          "<",
+        )
+        .from(mobileNavListRef.current.children, {
+          opacity: 0,
+          yPercent: 100,
+          stagger: 0.1,
+        });
+      timelineRef.current = tl;
     },
-    { scope: containerRef, dependencies: [isMobileNavOpen] },
+    { scope: containerRef },
   );
 
   const getActiveLinkClasses = (href: string) => {
@@ -88,20 +71,36 @@ const Navbar = () => {
     return "";
   };
 
-  const toggleMobileNav = () => {
+  const toggleMobileNav = contextSafe(() => {
+    const tl = timelineRef.current;
+    const overlay = mobileNavOverlayRef.current;
+    const nav = mobileNavRef.current;
+    const list = mobileNavListRef.current;
+
     const htmlElement = document.documentElement;
+
+    if (!tl || !overlay || !nav || !list) return;
 
     if (htmlElement.classList.contains("overflow-hidden"))
       htmlElement.classList.remove("overflow-hidden");
     else htmlElement.classList.add("overflow-hidden");
 
-    if (!displayMobileNav) {
-      setDisplayMobileNav(true);
-      setIsMobileNavOpen(true);
+    if (!isMobileNavOpen) {
+      overlay.classList.remove("hidden");
+      nav.classList.replace("hidden", "flex");
+      tl.play().eventCallback("onComplete", () => {
+        list.classList.replace("overflow-hidden", "overflow-auto");
+      });
     } else {
-      setIsMobileNavOpen(false);
+      list.classList.replace("overflow-auto", "overflow-hidden");
+      tl.reverse().eventCallback("onReverseComplete", () => {
+        overlay.classList.add("hidden");
+        nav.classList.replace("flex", "hidden");
+      });
     }
-  };
+
+    setIsMobileNavOpen((prev) => !prev);
+  });
 
   return (
     <section
@@ -113,61 +112,61 @@ const Navbar = () => {
         <p className="text-[20px] font-bold uppercase">Techcd</p>
 
         {/* Mobile nav toggle */}
-        <button onClick={toggleMobileNav} disabled={isAnimating}>
+        <button onClick={toggleMobileNav}>
           <NavMenuIcon className="w-[30px]" />
         </button>
       </div>
 
-      {displayMobileNav && (
-        <>
-          {/* Mobile nav overlay */}
+      <>
+        {/* Mobile nav overlay */}
+        <div
+          ref={mobileNavOverlayRef}
+          className="fixed inset-0 -z-[1] hidden bg-black/30 opacity-0"
+          onClick={toggleMobileNav}
+        ></div>
+
+        {/* Mobile nav */}
+        <nav
+          ref={mobileNavRef}
+          className="text-foreground fixed right-0 bottom-0 left-0 hidden max-h-[70dvh] overflow-hidden rounded-t-[2em] px-[2em] py-[3em] text-[20px]"
+        >
+          {/* Gradient bg */}
           <div
-            className={cn(
-              "fixed inset-0 -z-[1] bg-black/30",
-              isAnimating && "pointer-events-none",
-            )}
-            onClick={toggleMobileNav}
+            ref={mobileNavBgRef}
+            className="bg-background dark:from-background from-background absolute inset-0 -z-[1] bg-gradient-to-bl to-[#0A2A4A]"
+            style={{ clipPath: "circle(0% at 50% 100%)" }}
           ></div>
 
-          {/* Mobile nav */}
-          <nav className="text-foreground fixed right-0 bottom-0 left-0 flex max-h-[70vh] overflow-hidden rounded-t-[2em] px-[2em] py-[3em] text-[20px]">
-            {/* Gradient bg */}
-            <div
-              className="bg-background dark:from-background from-background mobile-nav-bg absolute inset-0 -z-[1] bg-gradient-to-bl to-[#0A2A4A]"
-              style={{ clipPath: "circle(0% at 50% 100%)" }}
-            ></div>
-
-            {/* Nav links */}
-            <ul className="mobile-nav-list flex grow flex-col gap-[2em] overflow-hidden pr-[8px]">
-              {navLinks.map(({ href, placeholder }) => (
-                <li
-                  key={href}
-                  className="mobile-nav-item bg-amber-300/0 text-center"
+          {/* Nav links */}
+          <ul
+            ref={mobileNavListRef}
+            className="flex grow flex-col gap-[2em] overflow-hidden pr-[8px]"
+          >
+            {navLinks.map(({ href, placeholder }) => (
+              <li key={href} className="text-center">
+                <Link
+                  href={href}
+                  className={cn("w-fit", getActiveLinkClasses(href))}
                 >
-                  <Link
-                    href={href}
-                    className={cn("w-fit", getActiveLinkClasses(href))}
-                  >
-                    {placeholder}
-                  </Link>
-                  <hr className="border-t-foreground mt-[0.3em] opacity-20" />
-                </li>
-              ))}
-
-              {/* Sign in/up */}
-              <li className="dark:text-primary mobile-nav-item text-background flex justify-center gap-[0.5em] font-medium">
-                <Link href="/sign-up">
-                  <button className="uppercase">Signup</button>
+                  {placeholder}
                 </Link>
-                <div className="via-background dark:via-foreground h-full w-[1px] bg-gradient-to-b from-transparent to-transparent"></div>
-                <Link href="/sign-in">
-                  <button className="uppercase">Login</button>
-                </Link>
+                <hr className="border-t-foreground mt-[0.3em] opacity-20" />
               </li>
-            </ul>
-          </nav>
-        </>
-      )}
+            ))}
+
+            {/* Sign in/up */}
+            <li className="dark:text-primary text-background flex justify-center gap-[0.5em] font-medium">
+              <Link href="/sign-up">
+                <button className="uppercase">Signup</button>
+              </Link>
+              <div className="via-background dark:via-foreground h-full w-[1px] bg-gradient-to-b from-transparent to-transparent"></div>
+              <Link href="/sign-in">
+                <button className="uppercase">Login</button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </>
     </section>
   );
 };
