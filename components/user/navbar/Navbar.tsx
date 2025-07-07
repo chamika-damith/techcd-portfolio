73ca9659bd245d3900ac4618788473695329513a
Dominic Brian <dominic91960@gmail.com>
2025-07-07 13:15:00 +0530

    feat: mobile navbar auth links

--- components/user/navbar/Navbar.tsx
+++ components/user/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ import NavMenuIcon from "../icons/NavMenuIcon";
 
 const Navbar = () => {
   const activePath = usePathname();
-  const [IsMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [IsMobileNavOpen, setIsMobileNavOpen] = useState(true);
 
   const getActiveLinkClasses = (href: string) => {
     const classes = "text-background dark:text-primary";
@@ -35,7 +35,10 @@ const Navbar = () => {
   return (
     <section className="fixed top-[1.5em] right-0 left-0 z-50">
       <div className="px-container container mx-auto flex items-center justify-between">
+        {/* Logo */}
         <p className="text-[20px] font-bold uppercase">Techcd</p>
+
+        {/* Mobile nav toggle */}
         <button onClick={toggleMobileNav}>
           <NavMenuIcon className="w-[30px]" />
         </button>
@@ -43,11 +46,15 @@ const Navbar = () => {
 
       {IsMobileNavOpen && (
         <>
+          {/* Mobile nav overlay */}
           <div
             className="fixed inset-0 -z-[1] bg-black/30"
             onClick={toggleMobileNav}
           ></div>
-          <nav className="from-background dark:to-background text-foreground fixed right-0 bottom-0 left-0 flex max-h-[70vh] rounded-t-[2em] bg-gradient-to-br to-[#0A2A4A] px-[2em] py-[3em] text-[20px] dark:from-[#0A2A4A]">
+
+          {/* Mobile nav */}
+          <nav className="bg-background text-foreground fixed right-0 bottom-0 left-0 flex max-h-[70vh] rounded-t-[2em] px-[2em] py-[3em] text-[20px] dark:from-[#0A2A4A]">
+            {/* Nav links */}
             <ul className="flex grow flex-col gap-[2em] overflow-auto pr-[8px]">
               {navLinks.map(({ href, placeholder }) => (
                 <li key={href} className="bg-amber-300/0 text-center">
@@ -60,6 +67,17 @@ const Navbar = () => {
                   <hr className="border-t-foreground mt-[0.3em] opacity-20" />
                 </li>
               ))}
+
+              {/* Sign in/up */}
+              <li className="dark:text-primary text-background flex justify-center gap-[0.5em] font-medium">
+                <Link href="/sign-up">
+                  <button className="uppercase">Signup</button>
+                </Link>
+                <div className="via-background dark:via-foreground h-full w-[1px] bg-gradient-to-b from-transparent to-transparent"></div>
+                <Link href="/sign-in">
+                  <button className="uppercase">Login</button>
+                </Link>
+              </li>
             </ul>
           </nav>
         </>
