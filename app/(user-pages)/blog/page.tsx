39c7cb977de9b7a1aff2d0564f44031e0b6bc226
Dominic Brian <dominic91960@gmail.com>
2025-07-18 18:37:41 +0530

    feat(blog-page): recent blog post section

--- app/(user-pages)/blog/page.tsx
+++ app/(user-pages)/blog/page.tsx
@@ -1,10 +1,13 @@
-import Header from "@/components/user/blog-page/header/Header";
 import React from "react";
+
+import Header from "@/components/user/blog-page/header/Header";
+import RecentBlogs from "@/components/user/blog-page/recent-blogs/RecentBlogs";
 
 const BlogPage = () => {
   return (
     <>
       <Header />
+      <RecentBlogs />
     </>
   );
 };
---
+++ components/user/blog-page/recent-blogs/RecentBlogs.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { ImArrowUpRight2 } from "react-icons/im";
+
+const RecentBlogs = () => {
+  return (
+    <section>
+      <div className="px-container container mx-auto pt-[2em]">
+        <p className="mb-[0.8em] text-[18px] font-medium uppercase sm:text-[20px] md:text-[21px] lg:text-[23px] xl:text-[25px] 2xl:text-[26px]">
+          Recent blog posts
+        </p>
+
+        <div className="grid grid-cols-1 gap-[1.5em] lg:grid-cols-2">
+          {/* START: Post 1 */}
+          <div className="grid grid-cols-1 gap-[1.5em] sm:grid-cols-2 sm:items-center lg:h-fit lg:grid-cols-1 lg:items-stretch">
+            <div className="bg-foreground/20 h-[160px] rounded-[0.8em] sm:h-[180px] md:h-[200px] lg:h-[220px] xl:h-[240px] 2xl:h-[260px]"></div>
+
+            <div className="flex flex-col gap-[0.5em]">
+              <p className="text-primary text-[12px] font-medium sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+                Sunday, 1 Jan 2023
+              </p>
+              <div className="flex items-center justify-between gap-[2em] text-[18px] sm:text-[20px] md:text-[21px] lg:text-[23px] xl:text-[25px] 2xl:text-[26px]">
+                <p className="line-clamp-1 pb-[0.1em]">
+                  Lorem Ipsum is simply dummy text of the
+                </p>
+                <ImArrowUpRight2 className="shrink-0" />
+              </div>
+              <p className="text-foreground/40 line-clamp-2 pb-[0.1em] text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[17px] 2xl:text-[18px]">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since
+              </p>
+              <div className="flex flex-wrap gap-[0.5em] text-[12px] sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+                <div className="text-primary rounded-full bg-[#EEF4FF] px-[1em] py-[0.4em]">
+                  Research
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* END: Post 1 */}
+
+          <div className="grid grid-cols-1 grid-rows-2 gap-[1.5em]">
+            {/* START: Post 2 */}
+            <div className="grid grid-cols-1 gap-[1.5em] sm:grid-cols-2 sm:items-center lg:items-stretch">
+              <div className="bg-foreground/20 h-[160px] rounded-[0.8em] sm:h-[180px] md:h-[200px] lg:h-[220px] xl:h-[240px] 2xl:h-[260px]"></div>
+
+              <div className="flex flex-col gap-[0.5em] lg:justify-center">
+                <p className="text-primary text-[12px] font-medium sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+                  Sunday, 1 Jan 2023
+                </p>
+                <div className="flex items-center justify-between gap-[2em] text-[18px] sm:text-[20px] md:text-[21px] lg:text-[23px] xl:text-[25px] 2xl:text-[26px]">
+                  <p className="line-clamp-1 pb-[0.1em]">
+                    Lorem Ipsum is simply dummy text of the
+                  </p>
+                  <ImArrowUpRight2 className="shrink-0" />
+                </div>
+                <p className="text-foreground/40 line-clamp-2 pb-[0.1em] text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[17px] 2xl:text-[18px]">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
+                  standard dummy text ever since
+                </p>
+                <div className="flex flex-wrap gap-[0.5em] text-[12px] sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+                  <div className="text-primary rounded-full bg-[#EEF4FF] px-[1em] py-[0.4em]">
+                    Research
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* END: Post 2 */}
+
+            {/* START: Post 3 */}
+            <div className="grid grid-cols-1 gap-[1.5em] sm:grid-cols-2 sm:items-center lg:items-stretch">
+              <div className="bg-foreground/20 h-[160px] rounded-[0.8em] sm:h-[180px] md:h-[200px] lg:h-[220px] xl:h-[240px] 2xl:h-[260px]"></div>
+
+              <div className="flex flex-col gap-[0.5em] lg:justify-center">
+                <p className="text-primary text-[12px] font-medium sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+                  Sunday, 1 Jan 2023
+                </p>
+                <div className="flex items-center justify-between gap-[2em] text-[18px] sm:text-[20px] md:text-[21px] lg:text-[23px] xl:text-[25px] 2xl:text-[26px]">
+                  <p className="line-clamp-1 pb-[0.1em]">
+                    Lorem Ipsum is simply dummy text of the
+                  </p>
+                  <ImArrowUpRight2 className="shrink-0" />
+                </div>
+                <p className="text-foreground/40 line-clamp-2 pb-[0.1em] text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[17px] 2xl:text-[18px]">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
+                  standard dummy text ever since
+                </p>
+                <div className="flex flex-wrap gap-[0.5em] text-[12px] sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+                  <div className="text-primary rounded-full bg-[#EEF4FF] px-[1em] py-[0.4em]">
+                    Research
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* END: Post 3 */}
+          </div>
+        </div>
+
+        {/* START: Post 4 */}
+        <div className="mt-[1.5em] grid grid-cols-1 gap-[1.5em] sm:grid-cols-2 sm:items-center">
+          <div className="bg-foreground/20 h-[160px] rounded-[0.8em] sm:h-[180px] md:h-[200px] lg:h-[220px] xl:h-[240px] 2xl:h-[260px]"></div>
+
+          <div className="flex flex-col gap-[0.5em]">
+            <p className="text-primary text-[12px] font-medium sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+              Sunday, 1 Jan 2023
+            </p>
+            <div className="flex items-center justify-between gap-[2em] text-[18px] sm:text-[20px] md:text-[21px] lg:text-[23px] xl:text-[25px] 2xl:text-[26px]">
+              <p className="line-clamp-1 pb-[0.1em]">
+                Lorem Ipsum is simply dummy text of the
+              </p>
+              <ImArrowUpRight2 className="shrink-0" />
+            </div>
+            <p className="text-foreground/40 line-clamp-2 pb-[0.1em] text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[17px] 2xl:text-[18px]">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry&apos;s standard dummy
+              text ever since
+            </p>
+            <div className="flex flex-wrap gap-[0.5em] text-[12px] sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[17px]">
+              <div className="text-primary rounded-full bg-[#EEF4FF] px-[1em] py-[0.4em]">
+                Research
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* END: Post 4 */}
+      </div>
+    </section>
+  );
+};
+
+export default RecentBlogs;
