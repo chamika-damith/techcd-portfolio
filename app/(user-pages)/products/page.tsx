ba5a5e631e5adb56e8b11ccbb58c3d3e7768b626
Dominic Brian <dominic91960@gmail.com>
2025-07-18 19:16:33 +0530

    feat(products-page): title section

---
+++ app/(user-pages)/products/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TitleArea from "@/components/user/product-page/TitleArea";
+
+const ProductPage = () => {
+  return (
+    <>
+      <TitleArea />
+    </>
+  );
+};
+
+export default ProductPage;
