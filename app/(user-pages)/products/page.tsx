c9d775f514b13ab09f3c13785ca9da7982aff589
Dominic Brian <dominic91960@gmail.com>
2025-07-18 19:18:24 +0530

    feat(products-page): product section compoent

--- app/(user-pages)/products/page.tsx
+++ app/(user-pages)/products/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TitleArea from "@/components/user/product-page/TitleArea";
+import ProductArea from "@/components/user/product-page/ProductArea";
 
 const ProductPage = () => {
   return (
     <>
       <TitleArea />
+      <ProductArea />
     </>
   );
 };
---
+++ components/user/product-page/ProductArea.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductArea = () => {
+  return (
+    <section>
+      <div className="px-container container mx-auto">ProductArea</div>
+    </section>
+  );
+};
+
+export default ProductArea;
