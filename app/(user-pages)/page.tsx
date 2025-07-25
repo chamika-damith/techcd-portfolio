5a095726fa47277b0a82983774f9329e0af3a8e7
Dominic Brian <dominic91960@gmail.com>
2025-07-25 10:04:04 +0530

    fix(home-page): banner changes

--- app/(user-pages)/page.tsx
+++ app/(user-pages)/page.tsx
@@ -22,7 +22,7 @@ const HomePage = () => {
           className="h-[188px] object-cover object-center sm:h-[281.75px] md:h-[375.5px] lg:h-[469.25px] xl:h-[563px] 2xl:h-[656.75px]"
         />
 
-        <div className="bg-foreground dark:bg-background absolute inset-0 flex items-center justify-center">
+        <div className="bg-foreground dark:bg-background absolute inset-0 hidden items-center justify-center">
           <button className="group text-background dark:text-foreground relative rounded-xl px-[1.5em] py-[0.6em] shadow-[0_0_1em_rgba(0,132,240,0.4)] transition-all duration-500 ease-linear hover:shadow-none">
             <span className="absolute inset-0 flex items-center justify-center rounded-xl">
               <span className="absolute inset-0 flex items-center justify-center overflow-hidden rounded-xl opacity-0 blur-[8px] transition-all duration-500 ease-linear group-hover:opacity-100">
