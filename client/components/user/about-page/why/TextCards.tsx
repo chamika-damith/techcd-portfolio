e1c246dbabdf50b1cec13a23ca35bea9eb20daa2
Dominic Brian <dominic91960@gmail.com>
2025-08-11 20:01:21 +0530

    fix(about-page): minor changes (#23)

--- client/components/user/about-page/why/TextCards.tsx
+++ client/components/user/about-page/why/TextCards.tsx
@@ -15,7 +15,7 @@ const TextCards = () => {
       const cn = container.current;
       if (!cn) return;
 
-      gsap.from(".text-card", {
+      gsap.from(".about-text-card", {
         duration: 1,
         opacity: 0,
         xPercent: -5,
@@ -40,10 +40,10 @@ const TextCards = () => {
       {Array.from({ length: 4 }).map((_, i) => (
         <div
           key={i}
-          className="bg-foreground/10 text-card border-foreground/20 rounded-[1em] border px-[1.5em] py-[1em] text-[13px] sm:text-[15px] md:text-[17px] lg:text-[19px] xl:text-[20px] 2xl:text-[21px]"
+          className="bg-foreground/10 about-text-card border-foreground/20 rounded-[1em] border px-[1.5em] py-[1em] text-[13px] sm:text-[15px] md:text-[17px] lg:text-[19px] xl:text-[20px] 2xl:text-[21px]"
         >
           <div className="flex items-center gap-[0.5em]">
-            <div className="size-[2em] rounded-full bg-[#D9D9D9]"></div>
+            <div className="bg-foreground/15 size-[2em] rounded-full"></div>
             <p className="text-[1.1em]">Lorem Ipsum</p>
           </div>
           <p className="mt-[0.5em] text-[0.9em] font-light">
